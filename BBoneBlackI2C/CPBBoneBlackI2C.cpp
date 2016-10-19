a27199d95aee237c1c25d50ca2c15835f794ed8e
HD <d@ptr0.com>
2016-10-19 17:21:55 +0300

    no message

--- BBoneBlackI2C/CPBBoneBlackI2C.cpp
+++ BBoneBlackI2C/CPBBoneBlackI2C.cpp
@@ -5,9 +5,9 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/ioctl.h>
-#include <linux/i2c-dev.h>
+#include <i2c-dev.h>
 #include <iostream>
-// #include "CPDebugMessage.hpp"
+
 
 using namespace std;
 
